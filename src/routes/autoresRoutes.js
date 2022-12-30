683df3411fe57f5f5c3993840ec4689e355ea416
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-12-29 22:05:37 -0300

    Projeto Final

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -4,9 +4,9 @@ import autoresController from "../controllers/autoresController.js";
 const router = express.Router();
 
 router.get("/autores", autoresController.listarAutores);
+router.get("/autores/:id", autoresController.listarAutor);
 router.post("/autores", autoresController.cadastrarAutores);
 router.put("/autores/:id", autoresController.atualizarAutor);
-router.get("/autores/:id", autoresController.listarAutor);
 router.delete("/autores/:id", autoresController.excluirAutor);
 
 export default router;
